7d94f9c2d0d6dd696c49df579378e83b3c6c1116
Matteo Cipolla <mcipolla@c2r5p4.42firenze.it>
2023-11-27 19:33:26 +0100

    update

---
+++ module04/ex00/Person.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Room;
+class Form;
+class Course;
+class Classroom;
+
+class Person
+{
+private:
+	std::string _name;
+	Room* _currentRoom;
+public:
+	Person(std::string p_name);
+	Room* room() {return (_currentRoom);}
+};
+
+class Student : public Person
+{
+private:
+	std::vector<Course*> _subscribedCourse;
+
+public:
+	void attendClass(Classroom* p_classroom);
+	void exitClass();
+	void graduate(Course* p_course);
+};
